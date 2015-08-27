157652a7d662a3e4d448d0c25388704bc05a8290
Adam Wick <awick@galois.com>
2015-08-27 15:51:10 -0700

    Make <sys/param.h> resolve to a real file.

---
+++ include/sys/param.h
@@ -0,0 +1,10 @@
+/* 
+ * Copyright (C) 2015, Galois, Inc.
+ * This sotware is distributed under a standard, three-clause BSD license.
+ * Please see the file LICENSE, distributed with this software, for specific
+ * terms and conditions.
+ */
+#ifndef MINLIBC_SYS_PARAM_H
+#define MINLIBC_SYS_PARAM_H
+
+#endif
