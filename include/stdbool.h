b69180a2fd54722b404fa2d8d9f9bf3cc177fff2
Adam Wick <awick@galois.com>
2017-01-23 09:50:37 -0800

    Add <stdbool.h>.

---
+++ include/stdbool.h
@@ -0,0 +1,14 @@
+/* 
+ * Copyright (C) 2017, Galois, Inc.
+ * This sotware is distributed under a standard, three-clause BSD license.
+ * Please see the file LICENSE, distributed with this software, for specific
+ * terms and conditions.
+ */
+#ifndef MINLIBC_STDBOOL_H
+#define MINLIBC_STDBOOL_H
+
+#define bool  _Bool
+#define true  1
+#define false 0
+
+#endif
